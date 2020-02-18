95d06fe252caa272af206246d733be2a48c5707e
Guillaume Le Vaillant <glv@posteo.net>
2020-02-18 10:50:07 +0100

    Add exception to license for OpenSSL.

--- src/bruteforce-wallet.c
+++ src/bruteforce-wallet.c
@@ -15,6 +15,16 @@
 
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  Additional permission under GNU GPL version 3 section 7
+
+  If you modify this program, or any covered work, by linking or combining
+  it with the OpenSSL library (or a modified version of that library),
+  containing parts covered by the terms of the OpenSSL license, the licensors
+  of this program grant you additional permission to convey the resulting work.
+  Corresponding source for a non-source form of such a combination shall include
+  the source code for the parts of the OpenSSL library used as well as that of
+  the covered work.
 */
 
 
